4ab25486b937a3897734b15e8b310389e9be736c
ayoolasolomon <ayoyink@yahoo.com>
2014-06-30 15:50:52 +0200

    Upadting the Javascript file

--- js/app.js
+++ js/app.js
@@ -8,10 +8,12 @@
 	 	console.log(numRand);
 	 	var current = null;
 	 	var previous = null;
- 	
+
+
+ 		// This is the submit Button for playing the game
  		$('#guess').click(function(){
  			
- 				var $userNum = $("#num").val();
+ 				var $userNum = parseInt($("#num").val());
  				var val = $userNum;
 
  					previous = current;
@@ -20,37 +22,33 @@
  				if (val > 100 || val < 1) {
  					$("#randomnumber").text("Please guess a number between 1 and 100");
  				} 
- 				else if ( val == numRand ) {
+ 				else if ( val === numRand ) {
  					$("#randomnumber").text("Congratulations! You got the Number");
  				}
  				else if ( current > numRand ) {
+ 					$("#randomnumber").text("You are Hot!");
  					if ( previous !== null && Math.abs( numRand - current ) < Math.abs( numRand - previous ) ) {
- 						$("#randomnumber").text("You are Hot");
+ 						$("#randomnumber").text("You are Hot!");
  					}
  					else if ( previous !== null && Math.abs( numRand - current ) > Math.abs( numRand - previous ) ) {
- 						$("#randomnumber").text("You are Cold");
+ 						$("#randomnumber").text("You are Cold!");
  					} 
- 					// else {
- 					// 	$("#randomnumber").text("Too High");
- 					// }
  				}
  				else if ( current < numRand ) {
+ 					$("#randomnumber").text("You are Hot!");
  					if ( previous !== null && Math.abs( numRand - current ) < Math.abs( numRand - previous ) ) {
- 						$("#randomnumber").text("You are Hot");
+ 						$("#randomnumber").text("You are Hot!");
  					}
  					else if ( previous !== null && Math.abs( numRand - current ) > Math.abs( numRand - previous ) ) {
- 						$("#randomnumber").text("You are Cold")
+ 						$("#randomnumber").text("You are Cold!")
  					}
- 					// else {
- 					// 	$("#randomnumber").text("Too Low");
- 					// }
  				}
  				else {
  					$("#randomnumber").text("This game can oly accept numbers");
  				}
 
  				$("#num").val('');
- 		});
+ 		});// Button for Guessing for the game ends here
 
 		// Button for a new game
  		$('#newgame').click(function() {
@@ -59,7 +57,7 @@
 
  		// Activates by pressing the enter key
  		$("#num").keyup(function(event){
-    		if(event.keyCode == 13){
+    		if(event.keyCode === 13){
         		$("#guess").click();
     		}
 		});
